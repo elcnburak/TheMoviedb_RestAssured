39d6894c1aedffa7803ec19ff6663046d050aa7e
elcnburak <elcnburak@hotmail.com>
2024-05-17 13:36:27 +0100

    _01_Valid_Login updated

--- src/test/java/_01_Valid_Login.java
+++ src/test/java/_01_Valid_Login.java
@@ -1,6 +1,10 @@
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
@@ -11,21 +15,33 @@ public class _01_Valid_Login {
 
     @Test
     public void _01_Valid_Login_Test() {
-        baseURI = "https://www.themoviedb.org";
-
-        // Your bearer token
-        String bearerToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NmY3NjY2ZWI1ZjRlNWMyYTVjNjk0NjQ5ZmNmMmM1NCIsInN1YiI6IjY2MzYyN2YzNjYxMWI0MDEyYTY3ZDdiNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BbnpQ7WOmRb1D73I4Ua7jgVSIePyPV3qT7uOGoxB3Ik";
+        baseURI = "https://api.themoviedb.org";
 
         reqSpec = new RequestSpecBuilder()
-                .addHeader("Authorization", "Bearer " + bearerToken)
                 .setContentType(ContentType.JSON)
+                .log(LogDetail.URI)
+                .build();
+
+        ResponseSpecification responseSpec = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .log(LogDetail.BODY)
+                .expectContentType(ContentType.JSON)
                 .build();
 
         given()
                 .spec(reqSpec)
-                .get("/login")
+                .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NmY3NjY2ZWI1ZjRlNWMyYTVjNjk0NjQ5ZmNmMmM1NCIsInN1YiI6IjY2MzYyN2YzNjYxMWI0MDEyYTY3ZDdiNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BbnpQ7WOmRb1D73I4Ua7jgVSIePyPV3qT7uOGoxB3Ik")
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/3/authentication")
                 .then()
-                .statusCode(200);
+                .log().all()
+                .spec(responseSpec)
+
+        ;
+
+
+
 
     }
 }
